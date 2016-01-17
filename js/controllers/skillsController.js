ded8358ea10653d8dfe88d4a19ae11fc4e22d053
Donal Hurley <djhurley1990@gmail.com>
2016-01-17 01:28:17 +0000

    Update skillsController.js

--- js/controllers/skillsController.js
+++ js/controllers/skillsController.js
@@ -1,5 +1,5 @@
 var skillsController = function($scope) {
-	var skillsArray = ['Java', 'Python', 'Bash', 'SQL', 'Javascript', 'HTML', 'CSS', 'JEE', 'JBoss', 'Git', 'Maven', 'Junit', 'Selenium', 'JPA', 'Hibernate', 'JAX-RS', 'MySQL', 'Backbone.js', 'JQuery', 'Apache Kafka', 'Apache Zookeeper', 'Apache Avro', 'Unity3D', 'Libgdx', 'Android SDK', 'Google Play Services', 'Ionic Framework', 'Angularjs', 'Angular Material', 'Bower'];
+	var skillsArray = ['Java', 'C#', 'Python', 'Bash', 'SQL', 'Javascript', 'HTML', 'CSS', 'JEE', 'JBoss', 'Git', 'Maven', 'Junit', 'Selenium', 'JPA', 'Hibernate', 'JAX-RS', 'MySQL', 'Backbone.js', 'JQuery', 'Apache Kafka', 'Apache Zookeeper', 'Apache Avro', 'Unity3D', 'Libgdx', 'Android SDK', 'Google Play Services', 'Ionic Framework', 'Angularjs', 'Angular Material', 'Bower'];
   $scope.skills = angular.copy(skillsArray);
 };
 
